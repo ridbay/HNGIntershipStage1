a3996d62b260044a17a0a934a953abb76ee17423
Balogun Ridwan Babatunde <balogunridwan@gmail.com>
2018-04-13 17:02:05 +0100

    Add files via upload

---
+++ hngstage1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+position: absolute;
+width: 100%;
+height: 100%;
+top: 70px;
+right: 400px;
+
+font-family: Roboto;
+font-style: normal;
+font-weight: normal;
+line-height: normal;
+font-size: 64px;
+text-align: center;
+
+color: #2F80ED;
+mix-blend-mode: normal;
+box-sizing: border-box;
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+img {
+position: absolute;
+width: 100%;
+height: 683px;
+
+background: url(Mark-Essien.jpg);
+}
+</style>
+</head>
+<body>
+
+	<img src="https://press.hotels.ng/wp-content/uploads/2016/02/Mark-Essien.jpg">
+	<div>Time: <?php echo date(" H:i:s", time()); 
+?>
+</div>
+
+
+</body>
+</html>
